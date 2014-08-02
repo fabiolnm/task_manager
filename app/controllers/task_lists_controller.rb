1b2b949950d7469c37b8334ef9d4523d5829cc08
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2014-08-01 22:54:04 -0300

    Add TaskLists scaffold controller with slim templates

---
+++ app/controllers/task_lists_controller.rb
@@ -0,0 +1,58 @@
+class TaskListsController < ApplicationController
+  before_action :set_task_list, only: [:show, :edit, :update, :destroy]
+
+  # GET /task_lists
+  def index
+    @task_lists = TaskList.all
+  end
+
+  # GET /task_lists/1
+  def show
+  end
+
+  # GET /task_lists/new
+  def new
+    @task_list = TaskList.new
+  end
+
+  # GET /task_lists/1/edit
+  def edit
+  end
+
+  # POST /task_lists
+  def create
+    @task_list = TaskList.new(task_list_params)
+
+    if @task_list.save
+      redirect_to @task_list, notice: t(:success)
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /task_lists/1
+  def update
+    if @task_list.update(task_list_params)
+      redirect_to @task_list, notice: t(:success)
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /task_lists/1
+  def destroy
+    @task_list.destroy
+    redirect_to task_lists_url, notice: t(:success)
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task_list
+    @task_list = TaskList.find params[:id]
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def task_list_params
+    params[:task_list].permit :name
+  end
+end
---
+++ test/controllers/task_lists_controller_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+
+describe TaskListsController do
+  let(:task_list) { task_lists :one }
+
+  it "gets index" do
+    get :index
+    assert_response :success
+    assigns(:task_lists).wont_be_nil
+  end
+
+  it "gets new" do
+    get :new
+    assert_response :success
+  end
+
+  it "creates task_list" do
+    lambda {
+      post :create, task_list: { name: 'A simple task' }
+    }.must_change ->{ TaskList.count }
+
+    assert_redirected_to task_list_path assigns :task_list
+  end
+
+  it "validates new task_list" do
+    lambda {
+      post :create, task_list: { name: '' }
+    }.wont_change ->{ TaskList.count }
+
+    assert_template :new
+  end
+
+  it "shows task_list" do
+    get :show, id: task_list
+    assert_response :success
+  end
+
+  it "gets edit" do
+    get :edit, id: task_list
+    assert_response :success
+  end
+
+  it "updates task_list" do
+    put :update, id: task_list, task_list: { name: 'A hard task' }
+    assert_redirected_to task_list_path assigns :task_list
+  end
+
+  it "validates existing task_list" do
+    put :update, id: task_list, task_list: { name: '' }
+    assert_template :edit
+  end
+
+  it "destroys task_list" do
+    lambda {
+      delete :destroy, id: task_list
+    }.must_change ->{ TaskList.count }, -1
+
+    assert_redirected_to :task_lists
+  end
+end
