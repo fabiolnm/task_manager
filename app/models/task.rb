19b8eff42086bb2dc02dcb1702199517147a232f
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2014-08-10 21:46:47 -0300

    Lists task_list's saved tasks on task_list#show view

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :task_list
 
   validates :task_list, :description, presence: true
+
+  def change_status_action
+    closed_at ? :reopen : :close
+  end
 end
